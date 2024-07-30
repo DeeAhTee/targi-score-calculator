c8c7fec56607d73e6d3b6e6e0060c452717a64d8
Bruce Truong <deeahtee@live.com>
2024-07-29 21:15:13 -0700

    minor comment edits

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ from welcome_message import welcome
 
 def calculate_score() -> None:
     """Calculate the score for the given player Class."""
-    print("Calculating score...")  # Feedback message  # noqa: T201
+    print("Calculating score...")  #noqa: T201
     # Implementation here
-    print("Score calculation completed.")  # Feedback message  # noqa: T201
+    print("Score calculation completed.") #noqa: T201
 
 
 def display_score_history() -> None:
     """Display the saved score history."""
-    print("Displaying score history...")  # Feedback message  # noqa: T201
+    print("Displaying score history...")  #noqa: T201
     # Implementation here
-    print("Score history displayed.")  # Feedback message  # noqa: T201
+    print("Score history displayed.")  #noqa: T201
 
 
 def display_menu(menu_list: list[str]) -> None:
     """Display the menu."""
-    print("Please select from the following or press any other key to exit:\n")  # noqa: T201
+    print("Please select from the following or press any other key to exit:\n")  #noqa: T201
     for i, menu_option in enumerate(menu_list):
-        print(f"{i+1}. {menu_option}")  # noqa: T201
+        print(f"{i+1}. {menu_option}")  #noqa: T201
 
 
 def get_user_choice(menu_list: list[str]) -> str:
@@ -41,7 +41,7 @@ def get_user_choice(menu_list: list[str]) -> str:
 
 def main() -> None:
     """Begin the Calculator."""
-    os.system("cls" if os.name == "nt" else "clear")  # OS-based clear # noqa: S605
+    os.system("cls" if os.name == "nt" else "clear")  # OS-based clear #noqa: S605
     welcome()
 
     menu_list = ["Calculate Score",
@@ -58,10 +58,10 @@ def main() -> None:
                 case "2":
                     display_score_history()
                 case _:
-                    print("\nExiting...")  # noqa: T201
+                    print("\nExiting...")  #noqa: T201
                     break
         else:
-            print("\nInvalid choice. Please try again.")  # noqa: T201
+            print("\nInvalid choice. Please try again.")  #noqa: T201
 
 if __name__ == "__main__":
     main()
