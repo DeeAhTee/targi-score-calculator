5c341efec2520ea8f1cf225b10aa627e83eefc1a
Olivia S <olivia.sausville@gmail.com>
2024-07-29 22:38:36 -0700

    Added welcome message and corrected warnings

--- welcome_message.py
+++ welcome_message.py
@@ -1,5 +1,6 @@
 """Module provides a function to display a welcome message."""
 
+
 def welcome() -> None:
     """Display welcome message."""
-    print("Olivia is steamin' hawt.\n")  # noqa: T201
+    print("Welcome to Targi Score Calculator\n")  # noqa: T201
